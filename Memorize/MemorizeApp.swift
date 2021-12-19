6e6c2ab5a968fc7b2f197fa1317be74c3d1b7ba4
Aidan Lee <aidanwlee@gmail.com>
2021-12-19 18:45:30 -0500

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Aidan Lee on 12/19/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
