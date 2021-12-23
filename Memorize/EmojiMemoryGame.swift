090bc784135faef59e22eebe036b55bb431c75dd
Aidan Lee <aidanwlee@gmail.com>
2021-12-23 09:51:44 -0500

    Lecture 5: Properties Layout

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -8,23 +8,26 @@
 import SwiftUI
 
 class EmojiMemoryGame: ObservableObject {
-    static let emojis = ["✈️","🚂","🚁","🚜","🚗","🚙","🚕","🚌","🚎","🏎","🚓","🚑","🚒","🚐","🛻","🚚","🚚","🚛","🚜","🛴","🛵","🏍","🛺","🚤"]
+    
+    typealias Card = MemoryGame<String>.Card
+    
+    private static let emojis = ["✈️","🚂","🚁","🚜","🚗","🚙","🚕","🚌","🚎","🏎","🚓","🚑","🚒","🚐","🛻","🚚","🚚","🚛","🚜","🛴","🛵","🏍","🛺","🚤"]
 
-    static func createMemoryGame() -> MemoryGame<String> {
+    private static func createMemoryGame() -> MemoryGame<String> {
         MemoryGame(numberOfPairsOfCards: 4) { pairIndex in
             emojis[pairIndex]
         }
     }
     
-    @Published private var model: MemoryGame<String> = createMemoryGame()
+    @Published private var model = createMemoryGame()
     
-    var cards: Array<MemoryGame<String>.Card> {
+    var cards: Array<Card> {
         model.cards
     }
     
     // MARK: - Intent(s)
     
-    func choose(_ card: MemoryGame<String>.Card) {
+    func choose(_ card: Card) {
         model.choose(card)
     }
 }
